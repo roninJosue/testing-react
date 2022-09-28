e0c6e6b869d680b3360ca91c574d5593da4d05ab
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-09-27 21:17:49 -0600

    test: Change App test

--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,8 @@ import App from './App';
 test('renders all buttons', () => {
   render(<App />);
 
-  const upButton = screen.getByText('up', {exact: false})
-  const dnButton = screen.getByText(/down/i)
+  const upButton = screen.getByText('+', {exact: false})
+  const dnButton = screen.getByText('-')
   const printButton = screen.getByText(/^print$/i)
 
   expect(upButton).toBeInTheDocument()
